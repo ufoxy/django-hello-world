74f8d52fb6501fb9ce301065ba0644558524a3c9
Arthur Bryan <arthurbryansk@gmail.com>
2024-10-31 16:57:54 -0300

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -77,12 +77,7 @@ WSGI_APPLICATION = 'api.wsgi.app'
 # Note: Django modules for using databases are not support in serverless
 # environments like Vercel. You can use a database over HTTP, hosted elsewhere.
 
-DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 
 # Password validation
